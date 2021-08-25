9fef0b14c7b7a0b6c4e575f3269e5db05533447a
TobyAdd <66429886+TobyAdd@users.noreply.github.com>
2021-08-25 23:04:43 +0300

    Delete Project v2 directory

--- Project v2/TobyFF/bin/Debug/render.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-
-cls
-title TobyFF [Rendering]
-ffmpeg -i "C:\Users\TobyAdd\Videos\wave_cursed.mp4" -vf "setpts=0.25*PTS" -filter:a "atempo=4",scale=640:360  "C:\Users\TobyAdd\Desktop\123123324234.mp4"
-title TobyFF [Done]
-Pause
